56294c615cf30628264c27eb2f6e503bf4f2cd8e
shawon-majid <shawon.majid@gmail.com>
2024-03-24 16:13:40 +0600

    server setup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,18 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config();
+import checkDatabaseConnection from './db/connection'
 
-console.log('hello world')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('EcoSync Server is Up...')
+})
+
+
+
+app.listen(PORT,async () => {
+    await checkDatabaseConnection()
+    console.log(`Server is running on PORT ${PORT}`)
+})
