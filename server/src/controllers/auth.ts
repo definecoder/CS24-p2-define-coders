3d96bca54ba05fbc922a9f42e247d866dde72c98
shawon-majid <shawon.majid@gmail.com>
2024-03-25 06:17:59 +0600

    nodemailer integrated for forget password functionality

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -2,8 +2,15 @@ import { Request, Response } from "express";
 import errorWrapper from "../middlewares/errorWrapper";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import { generateToken } from "../services/Token";
+import {
+  generateToken,
+  getToken,
+  invalidateToken,
+  verifyToken,
+} from "../services/Token";
 import CustomError from "../services/CustomError";
+import { randomOTPGenerator, randomPasswordGenerator } from "../services/utils";
+import { sendMail, sendOTPMail } from "../services/mailService";
 
 const prisma = new PrismaClient();
 
@@ -19,10 +26,13 @@ const createUser = errorWrapper(async (req: Request, res: Response) => {
     },
   });
 
-  const token = generateToken({
-    id: user.id,
-    role: user.roleName,
-  });
+  const token = generateToken(
+    {
+      id: user.id,
+      role: user.roleName,
+    },
+    "1h"
+  );
 
   res.status(201).json({ user, token });
 });
@@ -46,12 +56,80 @@ const login = errorWrapper(async (req: Request, res: Response) => {
     throw new Error("Invalid email or password");
   }
 
-  const token = generateToken({
-    id: user.id,
-    role: user.roleName,
-  });
+  const token = generateToken(
+    {
+      id: user.id,
+      role: user.roleName,
+    },
+    "1h"
+  );
 
   res.json({ user, token });
 });
 
-export { createUser, login };
+const logout = errorWrapper(async (req: Request, res: Response) => {
+  const token = getToken(req) || "";
+  invalidateToken(token);
+});
+
+const resetPasswordInit = errorWrapper(async (req: Request, res: Response) => {
+  const { email } = req.body;
+  const otp = randomOTPGenerator().toString();
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new CustomError("This email do not exists", 404);
+  }
+
+  const otptoken = generateToken({ id: user.id, OTP: otp }, "5m");
+
+  sendOTPMail(user, otp);
+
+  res.json({ otptoken });
+});
+
+const resetPasswordConfirm = errorWrapper(
+  async (req: Request, res: Response) => {
+    const { otp } = req.body;
+
+    const token = getToken(req) || "no token";
+
+    const decoded = verifyToken(token);
+
+    if ((decoded as any).OTP == otp) {
+      const { id } = decoded as any;
+
+      const newPassword = randomPasswordGenerator();
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+      const user = await prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          hashedPassword,
+        },
+      });
+
+      sendMail(
+        user,
+        `Reset Password Successful`,
+        `Your New Password is ${newPassword}`,
+        `Please change your password after login`
+      );
+
+      res.status(200).json({
+        msg: "OTP is correct!, your new password is sent to your email",
+      });
+    } else {
+      throw new CustomError("Wrong OTP", 403);
+    }
+  }
+);
+
+export { createUser, login, logout, resetPasswordInit, resetPasswordConfirm };
--- server/src/services/Token.ts
+++ server/src/services/Token.ts
@@ -1,11 +1,20 @@
 import jwt, { Secret } from "jsonwebtoken";
+import { Request } from "express";
 
-const generateToken = (info: any) => {
+const getToken = (req: Request) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return null;
+  }
+  return authHeader.split(" ")[1];
+};
+
+const generateToken = (info: any, expiry: string | number | undefined) => {
   const secret: Secret | undefined = process.env.JWT_SECRET;
   if (!secret) {
     throw new Error("JWT secret is undefined.");
   }
-  return jwt.sign(info, secret, { expiresIn: "1h" });
+  return jwt.sign(info, secret, { expiresIn: expiry });
 };
 
 const verifyToken = (token: string) => {
@@ -16,4 +25,10 @@ const verifyToken = (token: string) => {
   return jwt.verify(token, secret);
 };
 
-export { generateToken, verifyToken };
+const invalidateToken = (token: string) => {
+  // This is a dummy function that does nothing.
+  // In a real-world application, you would probably want to blacklist the token.
+  return;
+};
+
+export { generateToken, verifyToken, getToken, invalidateToken };
