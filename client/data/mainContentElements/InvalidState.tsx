f54fef7c7f80f2e4ad5e6e618ea1ec3c46130588
codermehraj <codermehraj@gmail.com>
2024-03-27 16:24:41 +0600

    fixed the hydration error in refresh

--- client/data/mainContentElements/InvalidState.tsx
+++ client/data/mainContentElements/InvalidState.tsx
@@ -2,11 +2,11 @@ export default function InvalidSate() {
     return (
       <div className="flex flex-1 max-h-[calc(100vh-60px)]">
         <div className="flex flex-col flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm text-lg text-gray-500">
-          <img src="/logoBlack.png" alt="Logo" className="h-1/6" />
+          <img src="/logoBlack.png" alt="Logo" className="w-24 h-24" />
           <h1 className="text-3xl  text-black font-semibold md:text-4xl m-2">
-            INVALID STATE
+            PLEASE WAIT
           </h1>
-          The state you are trying to access is invalid.
+          Proper access data is not loaded yet.
         </div>
       </div>
     );
