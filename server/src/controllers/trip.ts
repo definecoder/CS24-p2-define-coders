e7080ff557233b6156d62836bd12c5a81168dc2c
shawon-majid <shawon.majid@gmail.com>
2024-03-27 16:30:21 +0600

    land fill managers api and trip apis completed

--- server/src/controllers/trip.ts
+++ server/src/controllers/trip.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import errorWrapper from "../middlewares/errorWrapper";
-import { addTrip } from "../services/tripServices";
+import { addTrip, getTripById } from "../services/tripServices";
 import { Prisma, Trip } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 import CustomError from "../services/CustomError";
@@ -22,6 +22,16 @@ const createTrip = errorWrapper(async (req: Request, res: Response) => {
   if (!stsVehicleInfo) {
     throw new CustomError("No such Vehicle entry found", 404);
   }
+
+  await prisma.sTSVehicleEntry.update({
+    where: {
+      id: stsVehicleId,
+    },
+    data: {
+      exitTime,
+      weightOfWaste,
+    },
+  });
 
   const vehicle = stsVehicleInfo.vehicle;
   const sts = stsVehicleInfo.sts;
@@ -48,4 +58,57 @@ const createTrip = errorWrapper(async (req: Request, res: Response) => {
   res.status(201).json(newTrip);
 });
 
-export { createTrip };
+const getListOfTrips = errorWrapper(async (req: Request, res: Response) => {
+  const { tripStatus } = req.query;
+
+  let where: Prisma.TripWhereInput | undefined = undefined;
+
+  if (tripStatus) {
+    where = {
+      tripStatus: tripStatus as string,
+    };
+  }
+
+  const trips = await prisma.trip.findMany({
+    where,
+  });
+  res.json(trips);
+});
+
+const completeTrip = errorWrapper(async (req: Request, res: Response) => {
+  const { tripId, landfillId, vehicleId, weightOfWaste, entryTime } = req.body;
+
+  prisma.landfillVehicleEntry.create({
+    data: {
+      landfillId,
+      vehicleId,
+      weightOfWaste,
+      entryTime,
+    },
+  });
+
+  const trip = await getTripById(tripId);
+
+  if (!trip) {
+    throw new CustomError("No such trip found", 404);
+  }
+
+  const shortage = Number(trip.weightOfWaste) - weightOfWaste;
+
+  // calculate actual duration
+
+  const completedTrip = await prisma.trip.update({
+    where: {
+      id: tripId,
+    },
+    data: {
+      tripStatus: "COMPLETED",
+      weightOfWaste,
+      shortage,
+    },
+  });
+
+  res.json(completedTrip);
+});
+
+export { createTrip, getListOfTrips, completeTrip };
