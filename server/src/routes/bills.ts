ee5e547559f896990b887b8cb4701d744c71383f
shawon-majid <shawon.majid@gmail.com>
2024-03-28 01:29:21 +0600

    bill generation api done

--- server/src/routes/bills.ts
+++ server/src/routes/bills.ts
@@ -3,16 +3,20 @@ const router = express.Router();
 
 import {
   createBill,
+  createBillFromTrip,
   editBill,
   fetchBill,
   fetchBills,
+  getListOfBills,
   removeBill,
 } from "../controllers/bills";
 
 router.route("/").get(fetchBills);
+router.route("/search").get(getListOfBills);
 router.route("/create").post(createBill);
 router.route("/:billId").get(fetchBill);
 router.route("/:billId").put(editBill);
 router.route("/:billId").delete(removeBill);
+router.route("/create-from-trip/").post(createBillFromTrip);
 
 export default router;
