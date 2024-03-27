779c1d96a092ffc292658303438d697eec3b79f2
shawon-majid <shawon.majid@gmail.com>
2024-03-28 02:19:35 +0600

    fixed roles api

--- server/src/controllers/rbac.ts
+++ server/src/controllers/rbac.ts
@@ -7,7 +7,10 @@ const prisma = new PrismaClient();
 const getAllRoles = errorWrapper(
   async (req: Request, res: Response) => {
     const roles = await prisma.role.findMany({});
-    res.json(roles);
+
+    const roleNames = roles.map((role) => role.name);
+
+    res.json(roleNames);
   },
   { statusCode: 500, message: "Couldn't fetch roles" }
 );
