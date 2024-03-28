7a8d881fdd9d03216965d36a475f4ddaecf9897f
codermehraj <codermehraj@gmail.com>
2024-03-28 15:27:46 +0600

    Create new vehicle completed

---
+++ client/hooks/entityCreation/useCreateVehicle.ts
@@ -0,0 +1,50 @@
+"use client";
+import { apiRoutes } from "@/data/apiRoutes";
+import { jwtToken } from "@/data/cookieNames";
+import { admin, landfillManager, stsManager, unassigned } from "@/data/roles";
+import { getCookie } from "@/lib/cookieFunctions";
+import axios from "axios";
+export type Vehicle = {
+    vehicleNumber: string;
+    vehicleType: string;
+    capacity: number;
+    loadedFuelCostPerKm: number;
+    unloadedFuelCostPerKm: number;
+    landFillId: string;
+};
+
+export default function useCreateVehicle() {
+//   const [stsData, setStsData] = useState<STS>();
+
+  function isValid(vehicleData: Vehicle) {
+    
+    return (
+        vehicleData.vehicleNumber.length > 0 &&
+        vehicleData.vehicleType.length > 0 &&
+        vehicleData.capacity > 0 &&
+        vehicleData.loadedFuelCostPerKm !== null &&
+        vehicleData.unloadedFuelCostPerKm !== null &&
+        vehicleData.landFillId.length > 0
+    );
+  }  
+
+  async function createVehicle(vehicleData: Vehicle) {
+    if (vehicleData && isValid(vehicleData)) {
+      try {
+        const res = await axios.post(apiRoutes.vehicle.create, vehicleData, {
+          headers: {
+            Authorization: `Bearer ${await getCookie(jwtToken)}`,
+          },
+        });
+        window.location.reload();
+        return "Vehicle Aadded successfully";
+      } catch (error: any) {
+        return error.message?.toString() || "Error creating Vehicle. Do you have permisson?.";
+      }
+    }
+
+    return null;
+  }
+
+  return { createVehicle  };
+}
