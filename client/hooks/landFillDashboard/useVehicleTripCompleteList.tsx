518d559f44d5b3999c6017570135f9e8b437f2a7
nafi-ullah <shafinnafiullah@gmail.com>
2024-03-28 13:56:42 +0600

    vehicle trip complete list initiate

---
+++ client/hooks/landFillDashboard/useVehicleTripCompleteList.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { uri } from '@/data/constant';
+
+type Vehicle = {
+  tripId: string,
+  weightOfWaste: string,
+  vehicleNumber: string,
+  stsId: string,
+  vehicleType: string,
+  distance: string,
+  tripStartTime: string,
+  estimatedDuration: string
+  tripStatus: string
+  capacity: string,
+  
+};
+
+export default function useVehicleTripCompleteList() {  
+  const [vehicleList, setVehicleList] = useState<Vehicle[]>([]); // Initialize with an empty array of Vehicle objects
+  const [vehicleNumberList, setVehicleNumberList] = useState<string[]>([]); 
+
+  async function VehcileTripCompleteList() {
+    try {
+      const res = await axios.get('http://localhost:8585/trips/search?tripStatus=PENDING&landfillId=c4028362-6c17-4cf0-9b0e-ae20acfa2fbd');
+      // Assuming the response data is an array of vehicles
+      const AllVehicle: Vehicle[] = res.data.map((vehicle: any) => ({
+        tripId: vehicle.id,
+  weightOfWaste: `${vehicle.weightOfWaste} tons`,
+  vehicleNumber: vehicle.vehicle.vehicleNumber,
+  stsId: vehicle.sts.name,
+  vehicleType: vehicle.vehicle.vehicleType,
+  distance: `${vehicle.distance} km`,
+  tripStartTime: vehicle.tripStartTime,
+  estimatedDuration: `${vehicle.estimatedDuration} mins`,
+  tripStatus: vehicle.tripStatus,
+  capacity: vehicle.vehicle.capacity
+      }));
+     
+
+     setVehicleList(AllVehicle);
+     
+     
+      return true;
+    } catch (error: any) {
+      alert(error.message?.toString() || "Error fetching vehicle list");
+      return false;
+    }
+  }
+
+  return { vehicleList, VehcileTripCompleteList };
+}
