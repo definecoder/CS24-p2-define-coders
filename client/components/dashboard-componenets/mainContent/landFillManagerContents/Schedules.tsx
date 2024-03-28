68547918713b3070de8cac31be8550ca6f860d93
nafi-ullah <shafinnafiullah@gmail.com>
2024-03-28 06:09:42 +0600

    getVehicle List in dashboard

--- client/components/dashboard-componenets/mainContent/landFillManagerContents/Schedules.tsx
+++ client/components/dashboard-componenets/mainContent/landFillManagerContents/Schedules.tsx
@@ -1,12 +1,17 @@
 import EmptyFillContainer from "../../cards/EmptyFillContainer";
+import LanfFillUpcomingVehicles from "../../../dataTables/LandFillUpcomingVehicle"
 
 export default function LandfillManagerSchedules() {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6  max-h-[calc(100vh-60px)] overflow-scroll">
       <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
         <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 grid-flow-row gap-2 md:gap-4 w-full md:h-full max-h-max">
-        <div className="col-span-1 row-span-2 min-h-48">
-            <EmptyFillContainer>UPCOMING - CRITICAL FIRST SCHEDULES</EmptyFillContainer>
+        <div className="col-span-2 row-span-1 min-h-48">
+            <EmptyFillContainer>UPCOMING - CRITICAL FIRST SCHEDULES
+
+            <LanfFillUpcomingVehicles />
+            </EmptyFillContainer>
+            
           </div>
           <div className="col-span-1 row-span-1 min-h-48">
             <EmptyFillContainer>DISPATCH HISTORY</EmptyFillContainer>
