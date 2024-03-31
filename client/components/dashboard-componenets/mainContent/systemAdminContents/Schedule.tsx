6e5b6f735fd4d9c4d603fa34171bec9f875a9ff6
codermehraj <codermehraj@gmail.com>
2024-03-31 16:59:50 +0600

    schedule viewer done for admin

--- client/components/dashboard-componenets/mainContent/systemAdminContents/Schedule.tsx
+++ client/components/dashboard-componenets/mainContent/systemAdminContents/Schedule.tsx
@@ -1,20 +1,88 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
-import { Plus, Truck } from "lucide-react";
+import { CalendarSearchIcon } from "lucide-react";
 import EmptyFillContainer from "../../cards/EmptyFillContainer";
 import { BillBySTSId } from "@/components/dataTables/BillBySTSId";
+import useGetAllSTS from "@/hooks/dataQuery/useGetAllSTS";
+import { useState } from "react";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export default function AdminSchedulePanel() {
+  const { stsData } = useGetAllSTS();
+  const [selectedSTS, setSelectedSTS] = useState<string | null>();
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  function formatDate(dateString: string | undefined) {
+    if (!dateString) return undefined;
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2); // Months are 0 based, so +1 and pad with 0
+    const day = ("0" + date.getDate()).slice(-2);
+
+    return `${year}-${month}-${day}`;
+  }
+
   return (
     <main className="flex flex-1 flex-col p-4 lg:p-6 max-h-[calc(100vh-60px)] overflow-scroll items-center">
-      
-        <div>
-          SEE SCHEDULE FOR STS 1
-        </div>
+      <div className="flex justify-center gap-4 items-center mb-5 flex-wrap content-center">
+        SEE SCHEDULE FOR :
+        <Select
+          value={selectedSTS || ""}
+          onValueChange={(e) => {
+            setSelectedSTS(e);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a STS" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>STS</SelectLabel>
+              {stsData.map((sts) => (
+                <SelectItem key={sts.id} value={sts.id}>
+                  {sts.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>        
+        AND DATE :
+        <Popover>
+          <PopoverTrigger asChild>          
+            <Button variant={"outline"} className="flex gap-2">
+              <CalendarSearchIcon /> {formatDate(date?.toDateString()) || "Select a Date" }
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              className="rounded-md border"
+            />
+          </PopoverContent>
+        </Popover>        
+      </div>
 
-        <div className="flex flex-col items-center justify-center h-full w-full">
-          <BillBySTSId stsId="sts-1" />
-        </div>
-      
+      <div className="flex flex-col items-center justify-center w-full">
+        <BillBySTSId
+          stsId={selectedSTS || ""}
+          date={formatDate(date?.toDateString()) || ""}
+        />
+      </div>
     </main>
   );
 }
--- client/components/dataTables/BillBySTSId.tsx
+++ client/components/dataTables/BillBySTSId.tsx
@@ -9,54 +9,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { jwtToken, role } from "@/data/cookieNames";
 import { message } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import EmptyFillContainer from "../dashboard-componenets/cards/EmptyFillContainer";
+import { getCookie } from "@/lib/cookieFunctions";
+import { admin } from "@/data/roles";
+import { Button } from "../ui/button";
 
 const dummyDataArray: ScheduleEntry[] = [
   {
@@ -131,23 +91,29 @@ export function BillBySTSId({ stsId, date }: { stsId: string; date: string }) {
   const [stsName, setStsName] = useState<string>("");
 
   async function fetchStsScheduleData() {
+    if (!date || !stsId || stsId === "") {
+      return;
+    }
     try {
       // fetch data from api
       const res: any = await axios.get(
-        "http://localhost:8585/schedules/search?date=2024-01-5&stsId=sts1",
+        "http://localhost:8585/schedules/search?date=" +
+          date +
+          "&stsId=" +
+          stsId,
         {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            Authorization: `Bearer ${localStorage.getItem(jwtToken)}`,
           },
         }
       );
       const data = res.data.map((d: any) => ({
         time: d.scheduleTime,
-        vehicleNumber: d.vehicle?.vehicleNumber ?? 'N/A',
-        vehicleType: d.vehicle?.vehicleType ?? 'N/A',
-        landFillName: d.vehicle?.landFill?.name ?? 'N/A',
-        amount: d.wasteAmount ?? 'N/A',
-        duration: d.vehicle?.duration ?? 'N/A',
+        vehicleNumber: d.vehicle?.vehicleNumber ?? "N/A",
+        vehicleType: d.vehicle?.vehicleType ?? "N/A",
+        landFillName: d.vehicle?.landFill?.name ?? "N/A",
+        amount: d.wasteAmount ?? "N/A",
+        duration: d.vehicle?.duration ?? "N/A",
       }));
       setStsName(res.data[0].sts?.name);
       setSchedulData(data);
@@ -157,40 +123,73 @@ export function BillBySTSId({ stsId, date }: { stsId: string; date: string }) {
   }
 
   useEffect(() => {
+    setSchedulData([]);
     fetchStsScheduleData();
   }, []);
 
+  useEffect(() => {}, [scheduleData]);
+
+  useEffect(() => {
+    setSchedulData([]);
+    fetchStsScheduleData();
+  }, [date, stsId]);
+
   return (
-    <Table>
-      <TableCaption>Schedule of {stsName} for 12/12/2020.</TableCaption>
-      <TableHeader className="bg-slate-100 font-bold text-black text-md">
-        <TableRow>
-          <TableHead>Time</TableHead>
-          <TableHead>Vehicle Number</TableHead>
-          <TableHead>Vehicle Type</TableHead>
-          <TableHead>Landfill Name</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Duration</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {scheduleData.map((data, i) => (
-          <TableRow key={i}>
-            <TableCell>{data.time}</TableCell>
-            <TableCell>{data.vehicleNumber}</TableCell>
-            <TableCell>{data.vehicleType}</TableCell>
-            <TableCell>{data.landFillName}</TableCell>
-            <TableCell>{data.duration}</TableCell>
-            <TableCell>{data.amount}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={5}>Total</TableCell>
-          <TableCell>500 tons</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+    <>
+      {scheduleData[0] ? (
+        <div className="flex flex-col items-center justify-center w-full text-center">
+          <h1 className="font-bold text-2xl">EcoSync</h1>
+          <h6 className="italic text-md mt-2">Date: {date} </h6>
+          <h2 className="font-bold text-2xl"> SCHEDULE FOR WASTE TRANSFER </h2>
+          <h3 className="font-semibold text-lg mb-4"> {stsName} </h3>
+          <Table>
+            <TableCaption>
+              Schedule of {stsName} for {date}.
+            </TableCaption>
+            <TableHeader className="bg-slate-100 font-bold text-black text-md">
+              <TableRow>
+                <TableHead className="text-center">Time</TableHead>
+                <TableHead className="text-center">Vehicle Number</TableHead>
+                <TableHead className="text-center">Vehicle Type</TableHead>
+                <TableHead className="text-center">Landfill Name</TableHead>
+                <TableHead className="text-center">Duration</TableHead>
+                <TableHead className="text-center">Amount</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {scheduleData.map((data, i) => (
+                <TableRow key={i} className="text-center">
+                  <TableCell>{data.time}</TableCell>
+                  <TableCell>{data.vehicleNumber}</TableCell>
+                  <TableCell>{data.vehicleType}</TableCell>
+                  <TableCell>{data.landFillName}</TableCell>
+                  <TableCell>{data.duration} minutes</TableCell>
+                  <TableCell>{data.amount} tons</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={5} className="text-right font-bold">
+                  Total Waste :{" "}
+                </TableCell>
+                <TableCell>
+                  {scheduleData.reduce(
+                    (total, data) => total + parseInt(data.amount),
+                    0
+                  )}{" "}
+                  tons
+                </TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </div>
+      ) : (
+        <EmptyFillContainer className="flex justify-center items-center text-2xl font-bold text-center flex-col gap-5 min-h-96">
+          No schedule generated by Admin for {stsName} on {date}.
+          {getCookie(role) === admin && <Button> Generate Schedule </Button>}
+        </EmptyFillContainer>
+      )}
+    </>
   );
 }
