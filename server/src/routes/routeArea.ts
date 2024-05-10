f4450deeaee210b7c849a6910f76a85e029affa1
shawon-majid <shawonmajid@gmail.com>
2024-05-10 23:39:12 +0600

    route area api done

--- server/src/routes/routeArea.ts
+++ server/src/routes/routeArea.ts
@@ -10,11 +10,11 @@ import {
   addRouteToArea,
 } from "../controllers/routeArea";
 
-router.route("/create").post(addRoute);
-router.route("/").get(getAllRoutes);
-router.route("/area").post(addArea);
+router.route("/add-route").post(addRoute);
+router.route("/routes").get(getAllRoutes);
+router.route("/add-area").post(addArea);
 router.route("/area").get(getAllAreas);
-router.route("/:routeId").get(getRouteById);
+router.route("/routes/:routeId").get(getRouteById);
 router.route("/area/:areaId/route/:routeId").post(addRouteToArea);
 
 export default router;
