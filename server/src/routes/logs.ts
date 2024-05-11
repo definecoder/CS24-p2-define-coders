b740aeb25ed10c476eca434114143d0f756b60f5
shawon-majid <shawonmajid@gmail.com>
2024-05-11 10:24:55 +0600

    checking feature updated

--- server/src/routes/logs.ts
+++ server/src/routes/logs.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { getAdminLogs, getContractorManagerLogs } from "../controllers/logs";
-import { adminLog } from "../services/logdata";
+import { adminLog, contractorLog } from "../services/logdata";
 const router = express.Router();
 
 router.route("/checkin").post((req, res) => {
@@ -8,9 +8,13 @@ router.route("/checkin").post((req, res) => {
 
   if (flag) {
     adminLog("Checkin", "Shawon Majid has checked in");
+    contractorLog("Checkin", "Shawon Majid has checked in");
   } else {
+    contractorLog("Checkin", "Shawon Majid has checked in");
     adminLog("Checkout", "Shawon Majid has checked out");
   }
+
+  res.status(200).json({ message: "Checkin status updated" });
 });
 router.route("/admin").get(getAdminLogs);
 router.route("/contractor").get(getContractorManagerLogs);
